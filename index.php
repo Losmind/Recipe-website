385acac66c38f6b14343a2f636ad9dd4b7f72e2b
Losmind <vip.ilya2002@mail.ru>
2021-12-19 15:33:54 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>TopFood</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Hello world</h1>
+  </body>
+</html>
